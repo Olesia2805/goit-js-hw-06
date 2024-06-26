7f321b0f730aaafcb8281ca3241b169434c04b72
Olesia2805 <vysotina2002@gmail.com>
2024-06-26 23:42:11 +0200

    indexOf + splice

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+
+    #items;
+
+    constructor (items){
+        this.#items = items;
+    }
+
+    getItems(){
+        return this.#items;
+    }
+
+    addItem(newItem){
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove){
+        const whereRemovedItem = this.#items.indexOf(itemToRemove);
+        if (whereRemovedItem > -1) {
+            this.#items.splice(whereRemovedItem, 1)
+        }
+    }
+
+}
+
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
